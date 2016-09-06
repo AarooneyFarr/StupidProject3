8e6bdb939f7c50728d592f5bb7f715acc0fb7d36
AarooneyFarr <Aarooney.Farr@gmail.com>
2016-09-06 09:55:57 -0600

    made work?

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,5 +1,9 @@
 package stupid.controller;
 
 public class StupidController {
-
+   public static void main(String [] args){
+	   StupidController firstAppController = new StupidController();
+	   firstAppController.start();
+   }
 }
+
